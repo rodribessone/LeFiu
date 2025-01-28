18d93a758e908cfb0335adc36a5e46e8e9602363
rodribessone <rodrigo-bessone@hotmail.com>
2025-01-27 21:26:23 -0300

    cambio icono del nav

--- front/src/componentes/nav/Nav.jsx
+++ front/src/componentes/nav/Nav.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faHouse } from "@fortawesome/free-solid-svg-icons"
 import UserMenu from "../usuario/UserMenu"
 import Cart from "../HacerPedido/Cart"
 
 export default function Nav() {
   return (
     <div className='w-full bg-black text-white flex justify-between py-4 px-6 relative z-11'>
-        <Link to="/" className="text-2xl"><FontAwesomeIcon icon={faBars} /></Link>
+        <Link to="/" className="text-2xl"><FontAwesomeIcon icon={faHouse} /></Link>
         
         <h1  className="text-2xl font-bold italic font-cursive">Le Fiu</h1>
         
