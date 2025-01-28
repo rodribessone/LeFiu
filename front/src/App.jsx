991464cbf1bfd2e83ebbe87ef0f0f0be1c89b2f7
Juan Garcia <rodrigo-bessone@hotmail.com>
2025-01-28 19:24:00 -0300

    z index a footer y main

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -11,29 +11,26 @@ import Footer from './componentes/Footer/Footer'
 import ConfirmarCompra from './componentes/HacerPedido/ConfirmarCompra'
 
 function App() {
-  return (
+    return (
       <>
-          <div className="min-h-screen flex flex-col">
-              <img
-                  src="/Fondo.jpg"
-                  className="fixed z-1 object-cover w-full h-full"
-              />
-              {/* Contenedor principal */}
-              <div className="flex-grow flex flex-col relative z-10">
-                  <Nav />
-                  <Routes>
-                      <Route path="/" element={<Main />} />
-                      <Route path="/carta" element={<Carta />} />
-                      <Route path="/pedido" element={<Pedido />} />
-                      <Route path="/usuario" element={<Usuario />} />
-                      <Route path="/editarProducto/:id" element={<EditarProducto />} />
-                      <Route path="/crearProducto" element={<CrearProducto />} />
-                      <Route path="/confirmarCompra" element={<ConfirmarCompra />} />
-                  </Routes>
-              </div>
-              <Footer />
-          </div>
+      <div className='w-full h-[90vh]'>
+        <img src='/Fondo.jpg' className='fixed z-1 object-cover w-full h-full'/>
+        <div className='flex flex-col h-[100vh]'>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/carta" element={<Carta />} />
+          <Route path="/pedido" element={<Pedido />} />
+          <Route path="/usuario" element={<Usuario />} />
+          <Route path="/editarProducto/:id" element={<EditarProducto />} />
+          <Route path="/crearProducto" element={<CrearProducto />} />
+          <Route path="/confirmarCompra" element={<ConfirmarCompra />} />
+        </Routes>
+        <Footer />
+        </div>
+      </div>
       </>
-  );
+    )
 }
-export default App
+
+    export default App
