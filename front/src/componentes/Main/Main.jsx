354998c4ac40a18cda451285597a063c2e8d4545
rodribessone <rodrigo-bessone@hotmail.com>
2025-01-27 23:51:32 -0300

    hora x2

--- front/src/componentes/Main/Main.jsx
+++ front/src/componentes/Main/Main.jsx
@@ -16,7 +16,7 @@ export default function Main() {
 
     // Define horarios de apertura
     const schedule = {
-      1: { open: 20.5, close: 23.5 }, // Lunes: 20:30 - 23:30
+      1: { open: 20.5, close: 24 }, // Lunes: 20:30 - 23:30
       2: { open: 0, close: 23 }, // Martes: 20:30 - 23:30
       3: { open: 20.5, close: 23.5 }, // Miércoles: 20:30 - 23:30
       4: { open: 20.5, close: 23.5 }, // Jueves: 20:30 - 23:30
