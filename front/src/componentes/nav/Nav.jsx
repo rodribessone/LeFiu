ed4481243c6747bede2660bdd3f139bcf4d0db1a
rodribessone <rodrigo-bessone@hotmail.com>
2025-01-31 17:29:27 -0800

    cositas agregadas

--- front/src/componentes/nav/Nav.jsx
+++ front/src/componentes/nav/Nav.jsx
@@ -7,7 +7,7 @@ import Cart from "../HacerPedido/Cart"
 export default function Nav() {
   return (
     <div className='w-full bg-black fixed h-16 top-0 text-white flex justify-between py-4 px-6 z-30'>
-        <Link to="/" className="text-2xl"><FontAwesomeIcon icon={faHouse} /></Link>  { /* asdasd*/}
+        <Link to="/" className="text-2xl"><FontAwesomeIcon icon={faHouse} /></Link>  { /* aca metio algo re grande*/}
         
         <h1  className="text-2xl font-bold italic font-cursive">Le Fiu</h1>
         
