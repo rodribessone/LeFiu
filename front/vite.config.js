b3df5f10e3ad59400a0326b6a15a97db20c911f3
rodribessone <rodrigo-bessone@hotmail.com>
2025-02-01 15:26:55 -0800

    cambio package, vite.config y vercel

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",  // Asegura que la base de las rutas sea la raíz
+  build: {
+    outDir: "dist"
+  }
+});
