bc5e7ee9c905b298c4d1d0b649f82477959a1f8b
rodribessone <rodrigo-bessone@hotmail.com>
2025-02-01 15:37:21 -0800

    cambio viteconfig

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./",  // ⚠️ IMPORTANTE: Usa "./" en lugar de "/"
   build: {
     outDir: "dist"
   },
