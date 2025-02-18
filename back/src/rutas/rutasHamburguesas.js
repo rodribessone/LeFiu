ec3ce44bd8362d68848816514664598722a86113
rodribessone <rodrigo-bessone@hotmail.com>
2025-02-18 18:46:09 +1000

    verificacion de url

--- back/src/rutas/rutasHamburguesas.js
+++ back/src/rutas/rutasHamburguesas.js
@@ -4,7 +4,12 @@ const { getHamburguesasPrice, updateHamburguesasPrice } = require('../controlado
 const { authenticate, authorize } = require('../middleware/auth');
 
 // Ruta para obtener el precio extra para tipos de hamburguesa
-router.get('/hamburguesa', getHamburguesasPrice);
+console.log("Cargando rutas de hamburguesas...");
+router.get('/hamburguesa', (req, res) => {
+  console.log("Solicitud recibida en /hamburguesa");
+  getHamburguesasPrice(req, res);
+});
+console.log("Rutas de hamburguesas cargadas correctamente.");
 
 // Ruta para actualizar el precio extra para tipos de hamburguesa
 router.put('/hamburguesa', authenticate, authorize(['admin']), updateHamburguesasPrice);
