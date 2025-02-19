34a734a19ef39a9becda6fc31751a60338777634
rodribessone <rodrigo-bessone@hotmail.com>
2025-02-19 14:52:07 +1000

    me falto el import react je

--- front/src/componentes/HacerPedido/CopiarAlias.jsx
+++ front/src/componentes/HacerPedido/CopiarAlias.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";  // Agregar esta importación
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-regular-svg-icons";
 
